7f15faa7326a6de29805f7dec6bf6cc60e97e1ae
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-03-04 11:49:43 +0100

    Add security.cs

---
+++ App_Code/Security.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using WebMatrix.Data;
+
+
+/// <summary>
+/// Every action that takes place on an authentication basis.
+/// </summary>
+public static class Security
+{
+    /// <summary>
+    /// Gets random salt from bcrypt, then hashes password
+    /// </summary>
+    private static string HashPassword(string password)
+    {
+        //workfactor 13 seems the sweet spot ~1-2sec
+        var salt = BCrypt.Net.BCrypt.GenerateSalt(13);
+        return BCrypt.Net.BCrypt.HashPassword(password, salt);
+    }
+
+    /// <summary>
+    /// Creates an account
+    /// </summary>
+    public static string CreateAccount(string password)
+    {
+        return HashPassword(password);
+    }
+
+    /// <summary>
+    /// Login to blyat
+    /// </summary>
+    public static int Login(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Resets password and sends email with reset key
+    /// </summary>
+    public static bool ResetPassword()
+    {
+        //we need a webserver for this.
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Check for sessions
+    /// </summary>
+    public static bool IsAuthenticated(int session_id)
+    {
+        throw new NotImplementedException();
+    }
+}
